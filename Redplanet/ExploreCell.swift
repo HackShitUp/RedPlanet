4f113a6a79e08ccb6dce1c6bbe13bb2b9b072292
Josh M Choi <josh.m.choi@gmail.com>
2017-04-30 03:19:11 -0400

    • Removed Discover...

---
+++ Redplanet/ExploreCell.swift
@@ -0,0 +1,24 @@
+//
+//  ExploreCell.swift
+//  Redplanet
+//
+//  Created by Joshua Choi on 4/30/17.
+//  Copyright © 2017 Redplanet Media, LLC. All rights reserved.
+//
+
+import UIKit
+
+class ExploreCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ Redplanet/ExploreHeaderCell.swift
@@ -0,0 +1,24 @@
+//
+//  ExploreHeaderCell.swift
+//  Redplanet
+//
+//  Created by Joshua Choi on 4/30/17.
+//  Copyright © 2017 Redplanet Media, LLC. All rights reserved.
+//
+
+import UIKit
+
+class ExploreHeaderCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
